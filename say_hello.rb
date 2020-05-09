f68c34ad406e9ab1fc104cc9e42c7073fc35437e
Jean  Isidore <jnisidore@gmail.com>
2020-05-09 20:07:54 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,6 @@
-# Build your say_hello method here
+def say_hello greeting: "Hello" , programer: "Ruby programer"
+  puts greeting
+puts programer
+end
+
+say_hello
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
-require_relative '../say_hello'
-
-RSpec.configure do |config|
-  # config here
+def say_hello greeting: programer: "Hello Ruby programer!"
+puts programer
 end
+
+say_hello 
+
+Say_hello programer: "Gabriella"
+
